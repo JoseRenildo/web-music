d31c77f6eea34c1a546cc5681d8828a09401840a
José Renildo <renildojose7@gmail.com>
2024-03-25 16:04:43 -0300

    Update script.js
    Adicionando funcionalidades aos itens inteirados com o HTML (Barra de pesquisa, botão de pesquisa) e projeção para o botão 'Playlist'.

--- Interface_musical/script.js
+++ Interface_musical/script.js
@@ -55,23 +55,47 @@ const musicLibrary = [
     dontStay,
 ];
 
+let songs = [...musicLibrary];
+
+let playlist = [cumOnFeelTheNoize, dustToDust, dontStay]
+
 const pageBody = document.getElementById('page-body');
+const searchTerm = document.getElementById('search-term');
+const searchBtn = document.getElementById('search-button');
 
 function loadLibrary () {
     pageBody.innerHTML = '';
-    for(let index = 0; index < musicLibrary.length; index++) {
+    for(let index = 0; index < songs.length; index++) {
         pageBody.innerHTML += `
         <div class="card d-flex flex-column align-items-center" style="width: 18rem; height: 30rem;">
-            <img src="images/covers/Slipknot_capa.jpg" class="card-img-top" alt="...">
+            <img src="images/covers/${songs[index].coverFile}" class="card-img-top" alt="...">
             <div class="card-body d-flex flex-column">
-                <h5 class="card-title">Nome da musica</h5>
-                <p class="card-text">Nome do Álbum</p>
-                <p class="card-text">Nome do Artista</p>
-                <button class="btn btn-outline-info">Go somewhere</button>
+                <h5 class="card-title">${songs[index].songName}</h5>
+                <p class="card-text">${songs[index].album}</p>
+                <p class="card-text">${songs[index].artist}</p>
+                <button class="btn btn-outline-info"><i class="bi bi-plus-circle"></i></button>
             </div>
         </div>
     `;
         }
 }
+
+function searchClick () {
+    if (searchTerm.value === '') return;
+    songs = songs.filter(
+    (song) => song.songName.includes(searchTerm.value) ||
+    song.album.includes(searchTerm.value) ||
+    song.artist.includes(searchTerm.value))
+    loadLibrary()
+}
+
+function resetFilter () {
+    if (searchTerm.value !== '') return
+    songs = [...musicLibrary]
+    loadLibrary ()
+}
+
+searchBtn.addEventListener('click', searchClick)
+searchTerm.addEventListener('input', resetFilter)
 
 loadLibrary ()
